5a515ae351dd169d158eea554b47d8de3dd84c12
Quinn Baily <qbaily@arcadia.edu>
2022-06-02 13:40:29 -0400

    binded views from xml in MainActivity.kt

--- app/src/main/java/com/example/mtgdatahelper/MainActivity.kt
+++ app/src/main/java/com/example/mtgdatahelper/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.mtgdatahelper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val gameInHand = findViewById<TextView>(R.id.gih_val)
+        val gamePlayedWinRate = findViewById<TextView>(R.id.gp_wr_val)
+        val openingHandWinRate = findViewById<TextView>(R.id.oh_wr_val)
+        val gameInHandWinRate = findViewById<TextView>(R.id.gih_wr_val)
     }
 }
